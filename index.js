82146643e000fa0ef34a3b85f2dae12a50529f05
Tim de Pater <git@trafex.nl>
2016-01-08 15:11:46 +0100

    Added central heating and hotwater status to the metrics

--- index.js
+++ index.js
@@ -13,13 +13,17 @@ client.connect().then(function() {
 });
 
 function updateStats() {
-   Promise.try(function() {
+    Promise.try(function() {
         return [ client.status(), client.pressure() ];
     }).spread((status, pressure) => {
 
-        boilerIndicator = true;
-        if (status['boiler indicator'] == 'off') {
-            boilerIndicator = false;
+        var centralHeating = false;
+        var hotWater = false;
+        if (status['boiler indicator'] == 'central heating') {
+            centralHeating = true;
+        }
+        if (status['boiler indicator'] == 'hot water') {
+            hotWater = true;
         }
         body =
             'temp_setpoint,source=nefiteasy value=' + status['temp setpoint'].toFixed(2) + '\n' +
@@ -27,8 +31,8 @@ function updateStats() {
             'temp_outdoor,source=nefiteasy value=' + status['outdoor temp'].toFixed(2) + '\n' +
             'ps_active,source=nefiteasy value=' + status['ps active'] + '\n' +
             'fp_active,source=nefiteasy value=' + status['fp active'] + '\n' +
-            'boiler_indicator,source=nefiteasy value=' + boilerIndicator + '\n' +
-            'boiler_indicator,source=nefiteasy value=' + boilerIndicator + '\n' +
+            'centralheating,source=nefiteasy value=' + centralHeating + '\n' +
+            'hotwater,source=nefiteasy value=' + hotWater + '\n' +
             'pressure,source=nefiteasy value=' + pressure.pressure;
 
         request({
